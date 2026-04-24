3f9ec7bb88b54ceb11696493326c2c66c17525ce
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-04-24 14:06:25 +0200

    Update echo command to point to new URL

--- podman-run-wildfly.bash
+++ podman-run-wildfly.bash
@@ -1,3 +1,3 @@
 podman build --no-cache --rm --file Containerfile --tag wildfly:demo .
 podman run --interactive --tty --publish 0.0.0.0:8080:8080 wildfly:demo
-echo "browse https://localhost:8080/"
+echo "browse http://localhost:8080/hello.war/index.jsp?name=Test"
